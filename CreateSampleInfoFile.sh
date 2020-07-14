137670ff875fad0fceb4b3a47feba9205844dc19
hao zhang <542370159@qq.com>
2020-07-14 17:51:27 +0800

    test

--- CreateSampleInfoFile.sh
+++ CreateSampleInfoFile.sh
@@ -2,7 +2,7 @@
 
 tmp=(`date +"%Y%m%d%H%M%S"`)
 cat << EOF >temp_${tmp}.Sample_info.csv
-SampleID,SampleName,Group,Layout(PE/SE),batch(optional)
+SampleID,SampleName,Group,Layout(PE/SE),Batch(optional)
 R19051073,Hom1-enrich,Hom-enrich,PE,1st
 R19051077,Hom2-enrich,Hom-enrich,PE,2nd
 R19051085,Hom3-enrich,Hom-enrich,PE,2nd
