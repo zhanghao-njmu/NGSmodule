8f8ddc48cc1b3798441d148f32a84bd7f66ac24e
hao zhang <542370159@qq.com>
2020-08-02 19:08:18 +0800

    Update sra_download.sh

--- sra_download.sh
+++ sra_download.sh
@@ -3,6 +3,7 @@
 rawdata_dir=/data/database/SRR_collection/human/germ_cell_specification/rawdata/
 SRPfile=$(find $rawdata_dir -maxdepth 1 -name "meta.csv")
 IFS=','
+threads=1
 
 for file in ${SRPfile[@]};
 do
@@ -14,23 +15,31 @@ do
     if [[ $srr != "run_accession" ]];then
       {
       echo $srp $srr
-      prefetch --output-directory ${srp} --max-size 1000000000000 {}
-      cd $rawdata_dir/$srp/$srr
-      rm -rf ./fasterq.tmp*
-      fasterq-dump -f --split-3 ${srr}.sra -o $srr
-      if [ -e ${srr} ]; then
-        mv ${srr} ${srr}.fastq
+      
+      while [[ ! -e $rawdata_dir/$srp/$srr/$srr.sra ]] || [[ -e $rawdata_dir/$srp/$srr/$srr.sra.tmp ]] || [[ -e $rawdata_dir/$srp/$srr/$srr.sra.lock ]]
+      do
+        prefetch --output-directory ${srp} --max-size 1000000000000 $srr
+      done
+      
+      if [[ -e $rawdata_dir/$srp/$srr/$srr.sra ]] && [[ ! -e $rawdata_dir/$srp/$srr/$srr.sra.tmp ]] && [[ ! -e $rawdata_dir/$srp/$srr/$srr.sra.lock ]];then
+        cd $rawdata_dir/$srp/$srr
+        rm -rf ./fasterq.tmp*
+        fasterq-dump -f --threads $threads --split-3 ${srr}.sra -o $srr
+        if [ -e ${srr} ]; then
+          mv ${srr} ${srr}.fastq
+        fi
+        
+        ls ./ | grep -E "(*.fastq$)|(*.fq$)" | xargs -i pigz -f --processes $threads {}
+        
+        if [ -e ${srr}_2.fastq.gz ] ;then 
+          echo "$srp/$srr  pair-end"
+          reformat.sh in1=${srr}_1.fastq.gz in2=${srr}_2.fastq.gz vpair >/dev/null 2>&1 
+          [ $? -ne 0 ] && { echo -e "ERROR:${srr}_1.fastq.gz and ${srr}_2.fastq.gz have different numbers of reads!\n"; continue; } 
+        else
+          echo "$srp/$srr  single-end"
+        fi
       fi
       
-      ls ./ | grep -E "(*.fastq$)|(*.fq$)" | xargs -i pigz -f {}
-      
-      if [ -e ${srr}_2.fastq.gz ] ;then 
-        echo "$srp/$srr  pair-end"
-        reformat.sh in1=${srr}_1.fastq.gz in2=${srr}_2.fastq.gz vpair >/dev/null 2>&1 
-        [ $? -ne 0 ] && { echo -e "ERROR:${srr}_1.fastq.gz and ${srr}_2.fastq.gz appear to have different numbers of reads!\n"; continue; } 
-      else
-        echo "$srp/$srr  single-end"
-      fi
       }&
     fi
   done < $file
