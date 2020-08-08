3a48b5c1fd45768749db8bdbf71ee017fd197124
hao zhang <542370159@qq.com>
2020-08-08 19:25:47 +0800

    test

--- sra_download.sh
+++ sra_download.sh
@@ -33,7 +33,7 @@ do
         fi
 
         #if [[ ! $(grep "Names appear to be correctly paired" $dir/reformat_vpair.log) ]];then
-        if [[ -f $rawdata_dir/$srp/$srr/pigz.log ]];then
+        if [[ ! -f $rawdata_dir/$srp/$srr/pigz.log ]];then
           ls ./ | grep -E "(*.fastq$)|(*.fq$)" | xargs -i pigz -f --processes $threads {}
           echo -e "pigz finished">$rawdata_dir/$srp/$srr/pigz.log
         fi
