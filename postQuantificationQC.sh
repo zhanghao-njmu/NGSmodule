d0d3976b59e0cfb58950cc75416269e65b1d2b81
hao zhang <542370159@qq.com>
2020-08-12 15:09:56 +0800

    test

--- postQuantificationQC.sh
+++ postQuantificationQC.sh
@@ -3,14 +3,14 @@
 #######################################################################################
 trap 'trap - SIGTERM && kill -- -$$' SIGINT SIGTERM EXIT
 
-$Rscript &>/dev/null
+Rscript &>/dev/null
 [ $? -eq 127 ] && {
   color_echo "red" "Cannot find the command Rscript.\n"
   exit 1
 }
 R_packages=("limma" "edgeR" "data.table" "gplots" "stringr" "ComplexHeatmap" "ggsci" "ggpubr" "RColorBrewer" "circlize" "ggrepel" "GGally" "factoextra" "nord")
 for package in "${R_packages[@]}"; do
-  $Rscript -e "installed.packages()" | awk '{print $1}' | grep $package &>/dev/null
+  Rscript -e "installed.packages()" | awk '{print $1}' | grep $package &>/dev/null
   [ $? -ne 0 ] && {
     color_echo "red" "Cannot find the R package $package.\n"
     exit 1
@@ -28,7 +28,7 @@ SECONDS=0
 mkdir -p $maindir/NGSmodule_analysis/Quantification/postQuantificationQC
 cd $maindir/NGSmodule_analysis/Quantification/postQuantificationQC
 
-$Rscript $1 $maindir $Aligner $SampleInfoFile
+Rscript $1 $maindir $Aligner $SampleInfoFile
 
 ELAPSED="Elapsed: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
 echo -e "\n$ELAPSED"
