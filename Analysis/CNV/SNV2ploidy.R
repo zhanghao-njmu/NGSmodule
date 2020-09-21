194aec5fb85433c61faa264fd9be297f76b467d2
hao zhang <542370159@qq.com>
2020-09-21 21:20:54 +0800

    test

--- Analysis/CNV/SNV2ploidy.R
+++ Analysis/CNV/SNV2ploidy.R
@@ -29,8 +29,7 @@ plotlist <- list()
 color <- pal_material("blue-grey")(10)[c(3, 9, 1, 4)]
 
 chr_info <- readRDS(file = paste0(CNV_prefix, ".chr_info.rds"))
-p1 <- readRDS(file = paste0(CNV_prefix, ".p1.rds"))
-p2 <- readRDS(file = paste0(CNV_prefix, ".p2.rds"))
+plotlist <- readRDS(file = paste0(CNV_prefix, ".plotlist.rds"))
 
 ##### Allele balance #####
 chrom <- vcf@fix[, "CHROM"]
@@ -84,6 +83,7 @@ numeric_col <- c("Position", "Allele1", "Allele2", "Major_allele", "Minor_allele
 alleles[, numeric_col] <- sapply(alleles[, numeric_col], as.numeric)
 
 
+# define functions --------------------------------------------------------
 genetype_summary <- function(GeneType_table) {
   x <- GeneType_table
   is_het <- c(is.het(as.matrix(names(x))))
@@ -108,27 +108,42 @@ genetype_summary <- function(GeneType_table) {
   x <- x[!is.na(x)]
   label <- paste0(paste0("Het/Hom=", ratio, "\n"), paste0(" ", n, ": ", x, collapse = "\n"))
 
-  return(setNames(c(het, hom, as.numeric(ratio), label), c("het", "hom", "ratio", "label")))
+  return(setNames(
+    c(het, hom, as.numeric(ratio), label),
+    c("het", "hom", "ratio", "label")
+  ))
+}
+
+cutoff <- function(x, trim_low = 0.10, trim_high = 0.95) {
+  low_cutoff <- max(quantile(x, 0.25) - 1.5 * IQR(x), quantile(x, trim_low))
+  high_cutoff <- min(quantile(x, 0.75) + 1.5 * IQR(x), quantile(x, trim_high))
+  low_extend <- low_cutoff - 0.2 * (high_cutoff - low_cutoff)
+  high_extend <- high_cutoff + 0.2 * (high_cutoff - low_cutoff)
+  return(setNames(
+    c(low_cutoff, high_cutoff, low_extend, high_extend),
+    c("low_cutoff", "high_cutoff", "low_extend", "high_extend")
+  ))
 }
 
 # Allele Depth distribution -----------------------------------------------
-AD_q15 <- max(
-  quantile(alleles[["Allele1"]], probs = 0.15),
-  quantile(alleles[["Allele2"]], probs = 0.15)
-)
-AD_q95 <- max(
-  quantile(alleles[["Allele1"]], probs = 0.95),
-  quantile(alleles[["Allele2"]], probs = 0.95)
-)
-ad_df <- melt(alleles, measure.vars = c("Allele1", "Allele2"), variable.name = "Allele")
+AD_low <- cutoff(alleles[["Allele1"]])["low_cutoff"]
+AD_high <- cutoff(alleles[["Allele1"]])["high_cutoff"]
+low_x <- cutoff(alleles[["Allele1"]])["low_extend"]
+high_x <- cutoff(alleles[["Allele1"]])["high_extend"]
+
+ad_df <- reshape2::melt(alleles, measure.vars = c("Allele1", "Allele2"), variable.name = "Allele")
 p_AD <- ggplot() +
   geom_histogram(
-    data = subset(ad_df, value > 0 & value < AD_q95),
-    aes(x = value, fill = Allele), color = "black", alpha = 0.8
+    data = ad_df,
+    aes(x = value, fill = Allele),
+    color = "black", bins = 50, position = position_dodge(), center = 0.5
   ) +
-  geom_vline(xintercept = AD_q15, color = "red3", linetype = 2, size = 1) +
-  annotate("text", x = AD_q15, y = Inf, vjust = 1, hjust = 0, label = paste("q15:", AD_q15), color = "red3") +
+  geom_vline(xintercept = AD_low, color = "royalblue4", linetype = 2, size = 1) +
+  geom_vline(xintercept = AD_high, color = "red3", linetype = 2, size = 1) +
+  annotate("text", x = AD_low, y = Inf, vjust = 1, hjust = 0, label = paste("low-cutoff:", AD_low), color = "royalblue4") +
+  annotate("text", x = AD_high, y = Inf, vjust = 1, hjust = 1, label = paste("high-cutoff:", AD_high), color = "red3") +
   scale_fill_manual(name = "Allele", values = color[c(1, 2)]) +
+  scale_x_continuous(limits = c(low_x, high_x)) +
   labs(title = paste("Allele depth(AD):", sample), x = "", y = "Count") +
   theme_classic() +
   theme(
@@ -137,16 +152,23 @@ p_AD <- ggplot() +
   )
 
 # Depth distribution ------------------------------------------------------
-DP_q15 <- quantile(alleles[, "DP"], probs = 0.15)
-DP_q95 <- quantile(alleles[, "DP"], probs = 0.95)
+DP_low <- cutoff(alleles[["DP"]])["low_cutoff"]
+DP_high <- cutoff(alleles[["DP"]])["high_cutoff"]
+low_x <- cutoff(alleles[["DP"]])["low_extend"]
+high_x <- cutoff(alleles[["DP"]])["high_extend"]
+
 p_DP <- ggplot() +
   geom_histogram(
-    data = subset(alleles, DP > 0 & DP < DP_q95),
-    aes(x = DP, fill = is_het), color = "black", alpha = 0.8
+    data = alleles,
+    aes(x = DP, fill = is_het),
+    color = "black", bins = 50, position = position_dodge(), center = 0.5
   ) +
-  geom_vline(xintercept = DP_q15, color = "red3", linetype = 2, size = 1) +
-  annotate("text", x = DP_q15, y = Inf, vjust = 1, hjust = 0, label = paste("q15:", DP_q15), color = "red3") +
+  geom_vline(xintercept = DP_low, color = "royalblue4", linetype = 2, size = 1) +
+  geom_vline(xintercept = DP_high, color = "red3", linetype = 2, size = 1) +
+  annotate("text", x = DP_low, y = Inf, vjust = 1, hjust = 0, label = paste("low-cutoff:", DP_low), color = "royalblue4") +
+  annotate("text", x = DP_high, y = Inf, vjust = 1, hjust = 1, label = paste("high-cutoff:", DP_high), color = "red3") +
   scale_fill_manual(name = "Heterozygous", values = color[c(1, 2)]) +
+  scale_x_continuous(limits = c(low_x, high_x)) +
   labs(title = paste("Depth(DP):", sample), x = "", y = "Count") +
   theme_classic() +
   theme(
@@ -155,16 +177,21 @@ p_DP <- ggplot() +
   )
 
 # QUAL distribution -------------------------------------------------------
-QUAL_q15 <- quantile(alleles[, "QUAL"], probs = 0.15)
-QUAL_q95 <- quantile(alleles[, "QUAL"], probs = 0.95)
+QUAL_low <- cutoff(alleles[["QUAL"]])["low_cutoff"]
+QUAL_high <- cutoff(alleles[["QUAL"]])["high_cutoff"]
+low_x <- cutoff(alleles[["QUAL"]])["low_extend"]
+high_x <- cutoff(alleles[["QUAL"]])["high_extend"]
+
 p_QUAL <- ggplot() +
   geom_histogram(
-    data = subset(alleles, QUAL > 0 & QUAL < QUAL_q95),
-    aes(x = QUAL, fill = is_het), color = "black", alpha = 0.8
+    data = alleles,
+    aes(x = QUAL, fill = is_het),
+    color = "black", bins = 50, position = position_dodge(), center = 0.5
   ) +
-  geom_vline(xintercept = QUAL_q15, color = "red3", linetype = 2, size = 1) +
-  annotate("text", x = QUAL_q15, y = Inf, vjust = 1, hjust = 0, label = paste("q15:", QUAL_q15), color = "red3") +
+  geom_vline(xintercept = QUAL_low, color = "royalblue4", linetype = 2, size = 1) +
+  annotate("text", x = QUAL_low, y = Inf, vjust = 1, hjust = 0, label = paste("low-cutoff:", QUAL_low), color = "royalblue4") +
   scale_fill_manual(name = "Heterozygous", values = color[c(1, 2)]) +
+  scale_x_continuous(limits = c(low_x, high_x)) +
   labs(title = paste("Quality(QUAL):", sample), x = "", y = "Count") +
   theme_classic() +
   theme(
@@ -173,17 +200,19 @@ p_QUAL <- ggplot() +
   )
 
 # Mapping quality distribution --------------------------------------------
-MQ_q15 <- quantile(alleles[, "MQ"], probs = 0.15)
+MQ_low <- cutoff(alleles[["MQ"]])["low_cutoff"]
+MQ_high <- cutoff(alleles[["MQ"]])["high_cutoff"]
+low_x <- cutoff(alleles[["MQ"]])["low_extend"]
+high_x <- cutoff(alleles[["MQ"]])["high_extend"]
+
 p_MQ <- ggplot() +
   geom_histogram(
     data = alleles,
-    aes(x = MQ, fill = is_het), color = "black", alpha = 0.8
+    aes(x = MQ, fill = is_het),
+    color = "black", bins = 50, position = position_dodge(), center = 0.5
   ) +
-  geom_vline(xintercept = MQ_q15, color = "red3", linetype = 2, size = 1) +
-  annotate("text",
-    x = MQ_q15, y = Inf, vjust = 1, hjust = 1,
-    label = paste("q15:", MQ_q15), color = "red3"
-  ) +
+  geom_vline(xintercept = MQ_low, color = "royalblue4", linetype = 2, size = 1) +
+  annotate("text", x = MQ_low, y = Inf, vjust = 1, hjust = 1, label = paste("low-cutoff:", MQ_low), color = "royalblue4") +
   scale_fill_manual(name = "Heterozygous", values = color[c(1, 2)]) +
   labs(title = paste("Mapping quality:", sample), y = "Count") +
   theme_classic() +
@@ -195,8 +224,9 @@ p_MQ <- ggplot() +
 plotlist[["Alleles_QC"]] <- plot_grid(p_AD, p_DP, p_QUAL, p_MQ, nrow = 2)
 
 # subset alleles ----------------------------------------------------------
-alleles <- subset(alleles, (Allele1 >= AD_q15 | Allele2 >= AD_q15) &
-  DP > DP_q15 & QUAL > QUAL_q15 & MQ > MQ_q15)
+alleles <- subset(alleles, (Allele1 >= AD_low | Allele2 >= AD_low) &
+  (Allele1 <= AD_high & Allele2 <= AD_high) &
+  DP >= DP_low & DP <= DP_high & QUAL >= QUAL_low & MQ >= MQ_low)
 
 # all alleles -------------------------------------------------------------
 p_Chr <- ggplot(alleles) +
@@ -211,7 +241,7 @@ p_Chr <- ggplot(alleles) +
     strip.background = element_rect(fill = "grey85")
   )
 
-alleles_melt <- melt(alleles, measure.vars = c("Major_allele", "Minor_allele"), variable.name = "Type")
+alleles_melt <- reshape2::melt(alleles, measure.vars = c("Major_allele", "Minor_allele"), variable.name = "Type")
 p0 <- ggplot(data = alleles_melt) +
   geom_histogram(aes(x = value, y = ..density.., fill = Type), color = "black", bins = 50, alpha = 0.8) +
   geom_density(aes(x = value), fill = "grey80", color = "black", alpha = 0.5) +
@@ -286,7 +316,7 @@ gt_label_chr <- gt_label_chr %>%
     het_norm = het / reads,
     hom_norm = hom / reads
   )
-gt_label_chr_melt <- melt(gt_label_chr, measure.vars = c("hom_norm", "het_norm"), variable.name = "Type")
+gt_label_chr_melt <- reshape2::melt(gt_label_chr, measure.vars = c("hom_norm", "het_norm"), variable.name = "Type")
 
 
 p <- p0 + facet_wrap(. ~ chr, nrow = 4, ) +
@@ -329,7 +359,7 @@ p_ratio2 <- ggplot(data = gt_label_chr_melt, aes(x = chr, y = value, fill = Type
 plotlist[["HetHom_ratio"]] <- plot_grid(p_ratio1, p_ratio2, nrow = 1)
 
 # het alleles -------------------------------------------------------------
-het_alleles <- subset(alleles, Allele1 > AD_q15 & Allele2 > AD_q15 & is_het == TRUE)
+het_alleles <- subset(alleles, Allele1 >= AD_low & Allele2 >= AD_low & is_het == TRUE)
 
 p_Chr <- ggplot(het_alleles) +
   geom_bar(aes(x = chr, fill = is_het)) +
@@ -343,7 +373,7 @@ p_Chr <- ggplot(het_alleles) +
     strip.background = element_rect(fill = "grey85")
   )
 
-het_alleles_melt <- melt(het_alleles, measure.vars = c("Major_allele", "Minor_allele"), variable.name = "Type")
+het_alleles_melt <- reshape2::melt(het_alleles, measure.vars = c("Major_allele", "Minor_allele"), variable.name = "Type")
 p0 <- ggplot(data = het_alleles_melt) +
   geom_histogram(aes(x = value, y = ..density.., fill = Type),
     color = "black", bins = 50, alpha = 0.8
@@ -371,7 +401,7 @@ p_het <- p0 + annotate("text",
   x = 0.1, y = Inf,
   hjust = 0, vjust = 1.1
 ) +
-  labs(title = paste("AD(q15)-filtered heterozygous:", sample), subtitle = paste("Allele1 >", AD_q15, "&", "Allele2 >", AD_q15))
+  labs(title = paste("AD-filtered heterozygous:", sample), subtitle = paste("Allele1 >=", AD_low, "&", "Allele2 >=", AD_low))
 plotlist[["Het_alleles_frequency"]] <- plot_grid(p_het, p_Chr, nrow = 1)
 
 gt_chr <- het_alleles %>%
@@ -395,7 +425,7 @@ p <- p0 + facet_wrap(. ~ chr, nrow = 4) +
     x = 0.1, y = Inf, size = 3,
     hjust = 0, vjust = 1.1
   ) +
-  labs(title = paste("AD(q15)-filtered heterozygous:", sample), subtitle = paste("Allele1 >", AD_q15, "&", "Allele2 >", AD_q15))
+  labs(title = paste("AD-filtered heterozygous:", sample), subtitle = paste("Allele1 >=", AD_low, "&", "Allele2 >=", AD_low))
 plotlist[["Het_alleles_frequency_bychr"]] <- p
 
 
@@ -411,7 +441,7 @@ het_df <- subset(het_df, cum_pos < max(chr_info[, "chr_cum_end"]))
 het_df <- arrange(het_df, cum_pos)
 
 
-p3 <- ggplot(all_df) +
+p1 <- ggplot(all_df) +
   geom_vline(xintercept = pull(chr_info, "offset"), linetype = 1, color = "grey80", size = 0.5) +
   geom_point(
     aes(x = cum_pos, y = Minor_allele, color = chr_color),
@@ -439,7 +469,7 @@ p3 <- ggplot(all_df) +
     axis.ticks = element_blank()
   )
 
-p4 <- ggplot(melt(all_df, measure.vars = c("Major_allele", "Minor_allele"), variable.name = "Type")) +
+p2 <- ggplot(reshape2::melt(all_df, measure.vars = c("Major_allele", "Minor_allele"), variable.name = "Type")) +
   geom_histogram(
     aes(x = ..density.., y = value),
     bins = 50, color = "black", fill = color[4]
@@ -459,7 +489,7 @@ p4 <- ggplot(melt(all_df, measure.vars = c("Major_allele", "Minor_allele"), vari
     panel.grid.major.y = element_line(colour = "grey80", linetype = 2)
   )
 
-p5 <- ggplot(het_df) +
+p3 <- ggplot(het_df) +
   geom_vline(xintercept = pull(chr_info, "offset"), linetype = 1, color = "grey80", size = 0.5) +
   geom_point(
     aes(x = cum_pos, y = Minor_allele, color = chr_color),
@@ -486,7 +516,7 @@ p5 <- ggplot(het_df) +
     axis.ticks = element_blank()
   )
 
-p6 <- ggplot(melt(het_df, measure.vars = c("Major_allele", "Minor_allele"), variable.name = "Type")) +
+p4 <- ggplot(reshape2::melt(het_df, measure.vars = c("Major_allele", "Minor_allele"), variable.name = "Type")) +
   geom_histogram(
     aes(x = ..density.., y = value),
     bins = 50, color = "black", fill = color[4]
@@ -506,11 +536,11 @@ p6 <- ggplot(melt(het_df, measure.vars = c("Major_allele", "Minor_allele"), vari
     panel.grid.major.y = element_line(colour = "grey80", linetype = 2)
   )
 
-plot <- aplot::plot_list(list(p1, p2, p3, p4, p5, p6),
-  nrow = 3, ncol = 2,
-  widths = rep(c(0.9, 0.1), 3)
+plot <- aplot::plot_list(c(plotlist, list(p1, p2, p3, p4)),
+  nrow = 4, ncol = 2,
+  widths = rep(c(0.9, 0.1), 4)
 )
-ggsave(plot, filename = paste0(sample, ".plot.png"), width = nrow(chr_info) / 2, height = 2 * 3)
+ggsave(plot, filename = paste0(sample, ".plot.png"), width = nrow(chr_info) / 2, height = 2 * 4)
 
 
 ##### output report #####
@@ -526,77 +556,3 @@ invisible(dev.off())
 if (file.exists("Rplots.pdf")) {
   invisible(file.remove("Rplots.pdf"))
 }
-
-
-
-
-
-
-
-
-#
-# i <- 2
-# winsize <- windows*10
-# bin_width <- 0.02
-#
-# Peaks1 <- freq_peak(myMat = as.matrix(het_df[, "Major_allele"]),pos = het_df[,"cum_pos"], winsize = 1e6, bin_width = bin_width)
-# Peaks2 <- freq_peak(myMat = as.matrix(all_df[, "Minor_allele"]),pos = all_df[,"cum_pos"], winsize = 1e6, bin_width = bin_width)
-
-# layout(matrix(1:2, nrow = 1), widths = c(4, 1))
-# par(mar = c(5, 4, 4, 0))
-#
-# mySample <- "test"
-# plot(chr1_het_alleles$Position, chr1_het_alleles[, "Major_allele"],
-#   ylim = c(0, 1), type = "n", yaxt = "n",
-#   main = mySample, xlab = "POS", ylab = "Allele balance"
-# )
-# axis(
-#   side = 2, at = c(0, 0.25, 0.333, 0.5, 0.666, 0.75, 1),
-#   labels = c(0, "1/4", "1/3", "1/2", "2/3", "3/4", 1), las = 1
-# )
-# abline(h = c(0.25, 0.333, 0.5, 0.666, 0.75), col = 8)
-# points(chr1_het_alleles$Position,chr1_het_alleles[, "Major_allele"], pch = 20, col = "#A6CEE344")
-# points(chr1_het_alleles$Position,  chr1_het_alleles[, "Minor_allele"], pch = 20, col = "#1F78B444")
-# segments(
-#   x0 = myPeaks1$wins[, "START_pos"], y0 = myPeaks1$peaks[, 1],
-#   x1 = myPeaks1$wins[, "END_pos"], lwd = 3
-# )
-# segments(
-#   x0 = myPeaks1$wins[, "START_pos"], y0 = myPeaks2$peaks[, 1],
-#   x1 = myPeaks1$wins[, "END_pos"], lwd = 3
-# )
-# bp1 <- hist(chr1_het_alleles[, "Major_allele"], breaks = seq(0, 1, by = bin_width), plot = FALSE)
-# # Load libraries
-# library(vcfR)
-# library(pinfsc50)
-#
-# # Determine file locations
-# vcf_file <- system.file("extdata", "pinf_sc50.vcf.gz",
-#                         package = "pinfsc50")
-#
-# # Read data into memory
-# vcf <- read.vcfR(vcf_file, verbose = FALSE)
-#
-# ad <- extract.gt(vcf, element = 'AD')
-#
-# allele1 <- masplit(ad, record = 1)
-# allele2 <- masplit(ad, record = 2)
-#
-# ad1 <- allele1 / (allele1 + allele2)
-# ad2 <- allele2 / (allele1 + allele2)
-#
-# dp <- allele1
-# # sums <- apply(dp, MARGIN=2, quantile, probs=c(0.15, 0.95), na.rm=TRUE)
-# sums <- apply(dp, MARGIN = 2, quantile, probs = c(0.1, 0.9), na.rm = TRUE)
-#
-# par(mfrow = c(4, 3))
-# par(mar = c(2, 2, 1, 1))
-# par(oma = c(1, 1, 0, 0))
-#
-# for (i in 1:12) {
-#   hist(allele1[, i], breaks = seq(0, 1e3, by = 1), xlim = c(0, 100), col = 8, main = "", xlab = "", ylab = "")
-#   title(main = colnames(allele1)[i])
-#   abline(v = sums[, i], col = 2)
-# }
-# title(xlab = "Depth", line = 0, outer = TRUE, font = 2)
-# title(ylab = "Count", line = 0, outer = TRUE, font = 2)
