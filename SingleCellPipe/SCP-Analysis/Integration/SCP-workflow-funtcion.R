1f8e33d49e0e5ce6b0655357da9ec828ee7dfe45
hao zhang <542370159@qq.com>
2020-12-06 19:20:09 +0800

    test

--- SingleCellPipe/SCP-Analysis/Integration/SCP-workflow-funtcion.R
+++ SingleCellPipe/SCP-Analysis/Integration/SCP-workflow-funtcion.R
@@ -52,7 +52,7 @@ Standard_SCP <- function(sc, nHVF = 3000,
     sc[["CC.Difference"]] <- sc[["S.Score"]] - sc[["G2M.Score"]]
     sc[["Phase"]] <- factor(sc[["Phase", drop = TRUE]], levels = c("G1", "S", "G2M"))
   }
-  
+
   DefaultAssay(sc) <- "RNA"
   return(sc)
 }
@@ -499,8 +499,7 @@ Harmony_integrate <- function(sc_list, HVF_source = "separate", nHVF = 3000,
   sc_merge <- Reduce(function(x, y) merge(x, y), sc_list)
   DefaultAssay(sc_merge) <- "RNA"
   sc_merge <- NormalizeData(object = sc_merge) %>%
-    FindVariableFeatures(.) %>%
-    ScaleData(features = rownames(.))
+    FindVariableFeatures(.)
 
   if (HVF_source == "global") {
     # gene_common <- lapply(sc_list, rownames) %>% Reduce(intersect, .)
@@ -521,6 +520,7 @@ Harmony_integrate <- function(sc_list, HVF_source = "separate", nHVF = 3000,
     hvf <- SelectIntegrationFeatures(object.list = sc_list, nfeatures = nHVF)
   }
 
+  sc_merge <- ScaleData(object = sc_merge, features = rownames(sc_merge))
   sc_merge <- RunPCA(object = sc_merge, npcs = maxPC, features = hvf)
 
   srt_integrated <- RunHarmony(
@@ -706,6 +706,123 @@ Scanorama_integrate <- function(sc_list, HVF_source = "separate", nHVF = 3000,
   if ("tsne" %in% reduction) {
     srt_integrated <- RunTSNE(
       object = srt_integrated, reduction = "pca", dims = 1:PC_use, dim.embed = 2, tsne.method = "Rtsne",
+      perplexity = max(ceiling(ncol(srt_integrated) * 0.02), 30), max_iter = 2000, num_threads = 0, verbose = T
+    )
+  }
+
+  if (length(cc_S_genes) >= 3 & length(cc_G2M_genes) >= 3) {
+    srt_integrated <- CellCycleScoring(
+      object = srt_integrated,
+      s.features = cc_S_genes,
+      g2m.features = cc_G2M_genes,
+      set.ident = FALSE
+    )
+    srt_integrated[["CC.Difference"]] <- srt_integrated[["S.Score"]] - srt_integrated[["G2M.Score"]]
+    srt_integrated[["Phase"]] <- factor(srt_integrated[["Phase", drop = TRUE]], levels = c("G1", "S", "G2M"))
+  }
+
+  DefaultAssay(srt_integrated) <- "RNA"
+  return(srt_integrated)
+}
+
+CSS_integrate <- function(sc_list, HVF_source = "separate", nHVF = 3000,
+                          maxPC = 100, resolution = 0.8, reduction = c("tsne", "umap"),
+                          cc_S_genes = Seurat::cc.genes.updated.2019$s.genes, cc_G2M_genes = Seurat::cc.genes.updated.2019$g2m.genes,
+                          exogenous_genes = NULL) {
+  if (!HVF_source %in% c("global", "separate")) {
+    stop("'HVF_source' must be one of: 'global','separate'",
+      call. = FALSE
+    )
+  }
+
+  for (i in 1:length(sc_list)) {
+    DefaultAssay(sc_list[[i]]) <- "RNA"
+    if (identical(
+      x = GetAssayData(sc_list[[i]], slot = "counts"),
+      y = GetAssayData(sc_list[[i]], slot = "data")
+    )) {
+      sc_list[[i]] <- NormalizeData(object = sc_list[[i]])
+    }
+    if (length(VariableFeatures(sc_list[[i]])) == 0) {
+      sc_list[[i]] <- FindVariableFeatures(sc_list[[i]])
+    }
+    VariableFeatures(sc_list[[i]]) <- HVFInfo(sc_list[[i]]) %>%
+      filter(variance.standardized > 1 &
+        (!rownames(.) %in% exogenous_genes)) %>%
+      dplyr::arrange(desc(variance.standardized)) %>%
+      rownames(.) %>%
+      head(n = nHVF)
+  }
+
+  sc_merge <- Reduce(function(x, y) merge(x, y), sc_list)
+  DefaultAssay(sc_merge) <- "RNA"
+  sc_merge <- NormalizeData(object = sc_merge) %>%
+    FindVariableFeatures(.)
+
+  if (HVF_source == "global") {
+    # gene_common <- lapply(sc_list, rownames) %>% Reduce(intersect, .)
+    gene_common <- lapply(sc_list, function(x) {
+      m <- GetAssayData(x, assay = "RNA", slot = "counts")
+      gene_keep <- rownames(m)[Matrix::rowSums(m > 0) >= 5]
+      return(gene_keep)
+    }) %>% Reduce(intersect, .)
+    hvf <- HVFInfo(sc_merge) %>%
+      filter(variance.standardized > 1 &
+        (!rownames(.) %in% exogenous_genes) &
+        rownames(.) %in% gene_common) %>%
+      dplyr::arrange(desc(variance.standardized)) %>%
+      rownames(.) %>%
+      head(n = nHVF)
+  }
+  if (HVF_source == "separate") {
+    hvf <- SelectIntegrationFeatures(object.list = sc_list, nfeatures = nHVF)
+  }
+
+  sc_merge <- ScaleData(object = sc_merge, features = rownames(sc_merge))
+  sc_merge <- RunPCA(object = sc_merge, npcs = maxPC, features = hvf)
+
+  srt_integrated <- cluster_sim_spectrum(
+    object = sc_merge,
+    label_tag = "orig.ident",
+    cluster_resolution = resolution,
+    corr_method = "pearson",
+    spectrum_type = "corr_kernel"
+  )
+  sc_merge <- NULL
+
+  DefaultAssay(object = srt_integrated) <- "RNA"
+  if (identical(
+    x = GetAssayData(srt_integrated, slot = "counts"),
+    y = GetAssayData(srt_integrated, slot = "data")
+  )) {
+    srt_integrated <- NormalizeData(object = srt_integrated)
+  }
+  if (length(VariableFeatures(srt_integrated)) == 0) {
+    VariableFeatures(srt_integrated) <- hvf
+  }
+  if (nrow(GetAssayData(srt_integrated, slot = "scale.data")) == 0) {
+    srt_integrated <- ScaleData(object = srt_integrated, features = rownames(srt_integrated))
+  }
+
+  srt_integrated@project.name <- paste0(unique(srt_integrated[["orig.ident", drop = TRUE]]), collapse = ",")
+  srt_integrated[["orig.ident"]] <- factor(srt_integrated[["orig.ident", drop = TRUE]],
+    levels = unique(srt_integrated[["orig.ident", drop = TRUE]])
+  )
+
+  PC_use <- ncol(Embeddings(srt_integrated, reduction = "css"))
+  srt_integrated@misc$PC_use <- PC_use
+
+  srt_integrated <- FindNeighbors(object = srt_integrated, reduction = "css", dims = 1:PC_use, force.recalc = T)
+  srt_integrated <- FindClusters(object = srt_integrated, resolution = resolution, algorithm = 1, n.start = 100, n.iter = 10000)
+  srt_integrated <- BuildClusterTree(srt_integrated, features = hvf, slot = "scale.data", reorder = T, reorder.numeric = T)
+  srt_integrated$seurat_clusters <- Idents(srt_integrated)
+
+  if ("umap" %in% reduction) {
+    srt_integrated <- RunUMAP(object = srt_integrated, reduction = "css", dims = 1:PC_use, n.components = 2, umap.method = "uwot")
+  }
+  if ("tsne" %in% reduction) {
+    srt_integrated <- RunTSNE(
+      object = srt_integrated, reduction = "css", dims = 1:PC_use, dim.embed = 2, tsne.method = "Rtsne",
       perplexity = max(ceiling(ncol(srt_integrated) * 0.02), 30), max_iter = 2000, num_threads = 0, verbose = T
     )
   }
