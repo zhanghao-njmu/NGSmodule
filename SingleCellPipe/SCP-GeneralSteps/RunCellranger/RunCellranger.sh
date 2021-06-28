a3d5408dfeb6069d447e481d7c353ba99c149946
zh542370159 <542370159@qq.com>
2021-06-29 07:42:16 +0800

    test

--- SingleCellPipe/SCP-GeneralSteps/RunCellranger/RunCellranger.sh
+++ SingleCellPipe/SCP-GeneralSteps/RunCellranger/RunCellranger.sh
@@ -74,7 +74,7 @@ elif [[ ! -f $dropEst_config ]]; then
 fi
 
 echo -e "########################### RunCellranger Parameters ###########################\n"
-echo -e "  FastqScreen\n  FastqScreen_config: ${FastqScreen_config}\n"
+echo -e "  FastqScreen_config: ${FastqScreen_config}\n"
 echo -e "  cellranger_ref: ${cellranger_ref}"
 echo -e "  gene_gtf: ${gene_gtf}\n  rmsk_gtf: ${rmsk_gtf}\n  dropEst_config: ${dropEst_config}\n"
 echo -e "################################################################################\n\n"
